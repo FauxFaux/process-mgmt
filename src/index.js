3b0001f9de9a18284eb01eebf3dbfe0e19e60cb8
candle <candle@candle.me.uk>
2021-02-17 17:02:42 +0000

    display more of the ore processing chain

--- src/index.js
+++ src/index.js
@@ -1,7 +1,7 @@
 // import { data } from "./dsp/data.js"
 import { data } from "./factorio-ab/data.js"
-import { ProcessChain, Stack } from "./structures.js"
-import { inspect } from 'util'
+import { ProcessChain } from "./structures.js"
+// import { inspect } from 'util'
 
 
 // console.log(inspect(data, false, null, true));
@@ -9,6 +9,9 @@ import { inspect } from 'util'
 // console.log(data);
 
 let p = new ProcessChain([
+    data.processes.waste_water_purification_sulfuric,
+    data.processes.acid_sulfuric,
+    data.processes.gas_sulfur_dioxide,
     data.processes.iron_ore_by_sorting,
     data.processes.mineral_catalyst,
     data.processes.mineral_sludge_from_slag_slurry,
@@ -16,6 +19,11 @@ let p = new ProcessChain([
     data.processes.ore_jivolite_crushing,
     data.processes.ore_saphirite_crushing,
 ]);
+
+
+// let processes_from_exported_data = [
+//     'angelsore-crushed-mix1-processing', // iron ore from 2x crushed + catalyst.
+// ]
 
 //console.log(p.processes_by_output)
 // console.log("circuit production rate", data.processes[3].production_rate(data.items.circuit, 1))
