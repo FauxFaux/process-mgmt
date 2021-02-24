f153023a5e80b9625621c9970d68a549d38aacfe
candle <candle@candle.me.uk>
2021-02-24 17:20:57 +0000

    migrate dsp

--- src/dsp/data.js
+++ src/dsp/data.js
@@ -1,8 +1,8 @@
-import { Item, Stack, FactoryGroup, Factory, Process } from '../structures.js'
+import { Data, Item, Stack, FactoryGroup, Factory, Process } from '../structures.js'
 
-let data = {}
+let data = new Data("dsp", "0.0.1");
 
-data.items = [
+data.add_items([
     new Item("iron_ore", "iron ore"),
     new Item("iron_plate", "iron plate"),
     new Item("magnet", "magnet"),
@@ -14,65 +14,115 @@ data.items = [
     new Item("crystal_silicon", "crystal silicon"),
     new Item("processor", "processor"),
     new Item("microcrystalline_component", "microcrystalline component"),
-].reduce((acc, item) => {acc[item.id] = item; return acc;}, {});
+    new Item("electromagnetic_turbine", "electromagnetic turbine"),
+    new Item("graphine", "graphine"),
+    new Item("particle_container", "particle container"),
+    new Item("electric_motor", "electric motor"),
+    new Item("magnetic_coil", "magnetic coil"),
+    new Item("gear", "gear"),
+    new Item("a", ""),
+    new Item("b", ""),
+]);
 let items = data.items;
 
-data.factory_groups = [
+data.add_factory_groups([
     new FactoryGroup("smelter"),
     new FactoryGroup("assembler"),
-].reduce((acc, item) => {acc[item.id] = item; return acc;}, {});
+]);
 let factory_groups = data.factory_groups;
 
-data.processes = [
+data.add_processes([
     new Process(
+        "iron_plate",
         [new Stack(items.iron_ore, 1)],
         [new Stack(items.iron_plate, 1)],
         1,
         factory_groups.smelter
     ),
     new Process(
+        "magnet",
         [new Stack(items.iron_ore, 1)],
         [new Stack(items.magnet, 1)],
         1,
         factory_groups.smelter
     ),
     new Process(
+        "copper_plate",
         [new Stack(items.copper_ore, 1)],
         [new Stack(items.copper_plate, 1)],
         1,
         factory_groups.smelter
     ),
     new Process(
+        "high_purity_silicon",
         [new Stack(items.silicon_ore, 1)],
         [new Stack(items.high_purity_silicon, 1)],
         1,
         factory_groups.smelter
     ),
     new Process(
+        "crystal_silicon",
         [new Stack(items.high_purity_silicon, 1)],
         [new Stack(items.crystal_silicon, 1)],
         1,
         factory_groups.smelter
     ),
     new Process(
+        "circuit",
         [new Stack(items.copper_plate, 1), new Stack(items.iron_plate, 2)],
         [new Stack(items.circuit, 2)],
         1,
         factory_groups.assembler
     ),
     new Process(
+        "microcrystalline_component",
         [new Stack(items.copper_plate, 1), new Stack(items.high_purity_silicon, 2)],
         [new Stack(items.microcrystalline_component, 2)],
         2,
         factory_groups.assembler
     ),
     new Process(
+        "processor",
         [new Stack(items.microcrystalline_component, 2), new Stack(items.circuit, 2)],
         [new Stack(items.processor, 1)],
         3,
         factory_groups.assembler
     ),
-
-]
+    new Process(
+        "particle_container",
+        [new Stack(items.copper_plate, 2), new Stack(items.graphine, 2), new Stack(items.electromagnetic_turbine, 2)],
+        [new Stack(items.particle_container, 1)],
+        4,
+        factory_groups.assembler
+    ),
+    new Process(
+        "electromagnetic_turbine",
+        [new Stack(items.magnetic_coil, 2), new Stack(items.electric_motor, 2)],
+        [new Stack(items.electromagnetic_turbine, 1)],
+        2,
+        factory_groups.assembler
+    ),
+    new Process(
+        "magnetic_coil",
+        [new Stack(items.magnet, 2), new Stack(items.copper_plate, 1)],
+        [new Stack(items.magnetic_coil, 1)],
+        1,
+        factory_groups.assembler
+    ),
+    new Process(
+        "elctric_motor",
+        [new Stack(items.magnetic_coil, 1), new Stack(items.iron_plate, 2), new Stack(items.gear, 1)],
+        [new Stack(items.electric_motor, 1)],
+        2,
+        factory_groups.assembler
+    ),
+    new Process(
+        "gear",
+        [new Stack(items.iron_plate, 1)],
+        [new Stack(items.gear, 1)],
+        1,
+        factory_groups.assembler
+    ),
+]);
 
 export { data };
--- src/index.js
+++ src/index.js
@@ -1,5 +1,5 @@
-// import { data } from "./dsp/data.js"
-import { data } from "./factorio-ab/data.js"
+import { data } from "./dsp/data.js"
+//import { data } from "./factorio-ab/data.js"
 import { ProcessChain } from "./structures.js"
 // import { inspect } from 'util'
 
@@ -8,21 +8,38 @@ import { ProcessChain } from "./structures.js"
 
 // console.log(data);
 
-let p = new ProcessChain([
-    data.processes.waste_water_purification_sulfuric,
-    data.processes.acid_sulfuric,
-    data.processes.gas_sulfur_dioxide,
-    data.processes.iron_ore_by_sorting,
-    data.processes.mineral_catalyst,
-    data.processes.mineral_sludge_from_slag_slurry,
-    data.processes.slag_slurry_from_crushed_stone,
-    data.processes.ore_jivolite_crushing,
-    data.processes.ore_saphirite_crushing,
-]);
+// let p = new ProcessChain([
+//     data.processes.waste_water_purification_sulfuric,
+//     data.processes.acid_sulfuric,
+//     data.processes.gas_sulfur_dioxide,
+//     data.processes.iron_ore_by_sorting,
+//     data.processes.mineral_catalyst,
+//     data.processes.mineral_sludge_from_slag_slurry,
+//     data.processes.slag_slurry_from_crushed_stone,
+//     data.processes.ore_jivolite_crushing,
+//     data.processes.ore_saphirite_crushing,
+// ]);
 
+
+// Object.entries(data.processes).forEach(([id, proc]) => console.log(id));
+
+let names = [
+"iron_plate",
+"magnet",
+"gear",
+"magnetic_coil",
+"elctric_motor",
+"electromagnetic_turbine",
+"copper_plate",
+"particle_container",
+];
+let p = new ProcessChain(names.map(n => data.processes[n]));
+// let p = new ProcessChain(Object.entries(data.processes).flatMap(
+    //  ([id, proc]) => proc
+// ));
 
 // let processes_from_exported_data = [
-//     'angelsore-crushed-mix1-processing', // iron ore from 2x crushed + catalyst.
+    // 'angelsore-crushed-mix1-processing', // iron ore from 2x crushed + catalyst.
 // ]
 
 //console.log(p.processes_by_output)
