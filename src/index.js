2be0a3f83bb0cce6045b760f864866ad8e3941b4
candle <candle@candle.me.uk>
2021-04-18 17:31:29 +0100

    dsp full trees

--- src/index.js
+++ src/index.js
@@ -1,5 +1,5 @@
-// import { data } from './dsp/data.js';
-import { data } from './factorio-ab-01/data.js'
+import { data } from './dsp/data.js';
+// import { data } from './factorio-ab-01/data.js'
 //import { data } from './factorio-ab/data.js'
 import { ProcessChain, RateChain, Stack } from './structures.js';
 // import { inspect } from 'util'
@@ -29,11 +29,25 @@ const readline_disambiguate = function(requirement, options) {
 
 };
 
-const array_disambiguate = function(requirement, options) { }
-//const array_disambiguate = function(requirement, options) {
+// const array_disambiguate = function(requirement, options) { }
+const array_disambiguate = function(requirement, options) {
+    let arr = {};
+    arr[data.items.Diamond.id] = data.processes.Diamond_Rare;
+    arr[data.items.Carbon_Nanotube.id] = data.processes.Carbon_Nanotube;
+    arr[data.items.Graphene.id] = data.processes.Graphene_Rare;
+    arr[data.items.Casimir_Crystal.id] = data.processes.Casimir_Crystal;
+    arr[data.items.Photon_Combiner.id] = data.processes.Photon_Combiner;
+    arr[data.items.Energetic_Graphite.id] = data.processes.Energetic_Graphite;
+    arr[data.items.Crystal_Silicon.id] = data.processes.Crystal_Silicon_Rare;
+    if (arr[data.items.Diamond.id] === false) {
+        throw new Error('No enabled priority for ' + requirement + ' (available: ' + options.map(i => i.id).join(', ') + ')');
+    }
+    return arr[requirement];
+};
+// const array_disambiguate = function(requirement, options) {
 //     let arr = {};
-//     arr[data.items.diamond.id] = data.processes.diamond_rare;
-//     if (arr[data.items.diamond.id] === false) {
+//     arr[data.items.gas_nitrogen_monoxide.id] = data.processes.solid_sodium_nitrate_processing;
+//     if (!arr[requirement] === false) {
 //         throw new Error('No enabled priority for ' + requirement + ' (available: ' + options.map(i => i.id).join(', ') + ')');
 //     }
 //     return arr[requirement];
@@ -52,21 +66,27 @@ const array_disambiguate = function(requirement, options) { }
 // let p = new ProcessChain(names.map(n => data.processes[n]));
 // let p = new ProcessChain([data.processes['diamond']])
 let p = new ProcessChain(Object.values(data.processes))
+    .disable(data.processes.Sulphuric_Acid.id)
+    .disable(data.processes.Silicon_Ore.id)
+    .filter_for_output(new Stack(data.items.U_Matrix, 1), array_disambiguate)
     // .filter_for_output(new Stack(data.items.electric_motor, 1), array_disambiguate)
     // .filter_for_output(new Stack(data.items.graviton_lens, 1), array_disambiguate)
     // .filter_for_output(new Stack(data.items['liquid_sulfuric_acid'], 1), array_disambiguate)
     // .filter_for_output(new Stack(data.items.circuit, 1), array_disambiguate)
+    // .filter_for_output(new Stack(data.items.liquid_nitric_acid, 1), array_disambiguate)
     ;
 
-// let p = new RateChain(p1, {'assembler': data.factories.assembler_III});
-// let p = new RateChain(p1, {
+p = new RateChain(p, {'assembler': data.factories.assembler_III});
+// p = new RateChain(p, {
 //     'ore_sorting': data.factories['ore_crusher_3'],
 //     'ore_sorting_t1': data.factories['ore_sorting_facility_4'],
 //     'ore_sorting_t2': data.factories['ore_floatation_cell_3'],
 // });
 // let r = p.update(new Stack(data.items.circuit, 10));
 // p.update(new Stack(data.items['liquid_sulfuric_acid'], 600));
+// p.update(new Stack(data.items['liquid_nitric_acid'], 100));
 // p.update(new Stack(data.items.electric_motor, 8));
+p.update(new Stack(data.items.U_Matrix, 10), [data.items.Hydrogen.id]);
 // console.log(r[0], r[1]);
 // let p = new ProcessChain(Object.entries(data.processes).flatMap(
 //  ([id, proc]) => proc
@@ -82,3 +102,11 @@ let p = new ProcessChain(Object.values(data.processes))
 // console.log(p.require_output(new Stack(data.items.circuit, 10)));
 // console.log(p.all_items());
 console.log(p.to_graphviz());
+
+
+// TITANIUM
+// ALUMINIUM (bauxite)
+// SILVER
+// COBALT
+
+// (URANIUM)
