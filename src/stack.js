e88ad3fe9b8a9ff85e234ca5f840eb868d9b216c
candle <candle@candle.me.uk>
2021-05-02 22:33:11 +0100

    stack subtractions, powers. stack set effective differnces bettween +ives and -ives.

--- src/stack.js
+++ src/stack.js
@@ -19,11 +19,17 @@ class Stack {
             return this.clone();
         }
     }
+    sub(other) {
+        return this.add(other.mul(-1));
+    }
     div(scalar) {
         return new Stack(this.item, this.quantity / scalar);
     }
     mul(scalar) {
         return new Stack(this.item, this.quantity * scalar);
+    }
+    pow(scalar) {
+        return new Stack(this.item, this.quantity ** scalar);
     }
     toString() {
         return 'Stack: [item:'+this.item.id+', quantity: '+this.quantity+']';
@@ -50,15 +56,82 @@ class StackSet {
     total(item) {
         return this.stacks[item.id].reduce((p, c) => p.add(c), new Stack(item, 0));
     }
+
+    max_total(ignoring = []) {
+        // Find the item type that has the largest +ive total. Return that stack.
+        return this._min_max_total(ignoring, (a, b) => a.quantity > b.quantity);
+    }
+    min_total(ignoring = []) {
+        // Find the item type that has the largest -ive total. Return that stack.
+        return this._min_max_total(ignoring, (a, b) => a.quantity < b.quantity);
+    }
+    _min_max_total(ignoring, fn) {
+        return Object.keys(this.stacks)
+            // .map(e => {console.log(e); return e;})
+            .filter(i => !ignoring.includes(i))
+            .map(id => this.total(this.stacks[id][0].item))
+            // .map(e => {console.log(e); return e;})
+            .reduce( (acc, cur) => {
+                if (fn(acc, cur)) return acc;
+                return cur;
+            } )
+    }
+
+    // calculate the error margins for each stack type
+    // Relative difference between the inputs and outputs.
+    // Used to find which type has the largest difference
+    // between the +ives and -ives irrelevent of the
+    // magnitudes: e.g.
+    // type A has +3 & -4.
+    // type B has +50 & -60.
+    // Type A has the larger effective difference.
+    margins(ignoring = []) {
+        return Object.keys(this.stacks)
+            .filter(i => !ignoring.includes(i))
+            .map(id => this.stacks[id][0].item)
+            .map(item => {
+                let total_p = this.total_positive(item);
+                let total_n = this.total_negative(item);
+                let total = total_p.add(total_n);
+                let margin = total.div(total_p.sub(total_n).quantity); // m = total / (p + (-1*n))
+                return margin;
+            })
+            .reduce((acc, m) => {acc.add(m); return acc;}, new StackSet());
+    }
+
+    // See 'margins'; except the values are squared to get the magnitudes.
+    margins_squared(ignoring = []) {
+        return Object.keys(this.stacks)
+            .filter(i => !ignoring.includes(i))
+            .map(id => this.stacks[id][0].item)
+            .map(item => {
+                let total_p = this.total_positive(item);
+                let total_n = this.total_negative(item);
+                let total = total_p.add(total_n);
+                let margin = total.div(total_p.sub(total_n).quantity); // m = total / (p + (-1*n))
+                return margin;
+            })
+            .map(m => m.pow(2))
+            .reduce((acc, m) => {acc.add(m); return acc;}, new StackSet());
+    }
+
     total_positive(item) {
-        return this.stacks[item.id]
+        if (this.stacks[item.id]) {
+            return this.stacks[item.id]
             .filter(s => s.quantity > 0)
             .reduce((p, c) => p.add(c), new Stack(item, 0));
+        } else {
+            return new Stack(item, 0);
+        }
     }
     total_negative(item) {
-        return this.stacks[item.id]
-            .filter(s => s.quantity < 0)
-            .reduce((p, c) => p.add(c), new Stack(item, 0));
+        if (this.stacks[item.id]) {
+            return this.stacks[item.id]
+                .filter(s => s.quantity < 0)
+                .reduce((p, c) => p.add(c), new Stack(item, 0));
+        } else {
+            return new Stack(item, 0);
+        }
     }
 }
 
