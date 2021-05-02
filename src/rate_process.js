feed7a1fd2e65725f44ccfe52c7ffc2d9c63d95b
candle <candle@candle.me.uk>
2021-05-02 22:33:54 +0100

    various attempts to calculate ratios with process loops.

--- src/rate_process.js
+++ src/rate_process.js
@@ -3,6 +3,12 @@ import { Factory } from './factory.js';
 import { Process, ProcessChain } from './process.js';
 
 
+import * as fs from 'fs'
+import * as path from 'path'
+import { fileURLToPath } from 'url';
+const __dirname = path.dirname(fileURLToPath(import.meta.url));
+
+
 class RateProcess extends Process {
     constructor(p, factory_type) {
         let p_ = factory_type.update_process(p);
@@ -64,15 +70,245 @@ class RateChain extends ProcessChain {
                     // have 2 already, need 6 for this, then push 4 onto the queue. subtract 6 from the materials.
                     // have -5 already, need 7 for this, then push 7 onto the queue. subtract 7 from the materials.
                     // ==> subtract from materials. if total <= 0, push onto queue.
-                    materials.sub(input.mul(process_count));
-                    if (materials.total(input.item).quantity <= 0) {
+                    let required_for_count = input.mul(process_count);
+                    materials.sub(required_for_count);
+                    let remaining_required = materials.total(input.item);
+                    if (remaining_required.quantity <= 0) {
                         if (!imported_materials.includes(input.item.id)) {
-                            queue.push(input.mul(process_count));
+                            queue.push(remaining_required.mul(-1));
                         }
                     }
                 });
             }
         }
+        this.materials = materials;
+        this.process_counts = process_counts;
+    }
+
+    // target is to produce stack
+    // imported_materials are allowed to have any -ive material count.
+    // exported_materials are allowed to have any +ive material count.
+    update2(stack, imported_materials, exported_materials) {
+        let materials = new StackSet();
+        materials.sub(stack);
+        let process_counts = {};
+
+        let current = materials.min_total(imported_materials);
+        while (current.quantity < 0) {
+            // deal with 'current'
+            let processes = this.processes_by_output[current.item.id];
+            if (processes.length > 1) {
+                throw new Error("p > 1");
+            } else {
+                process = processes[0];
+                if (!process_counts[process.id]) { process_counts[process.id] = 0; }
+                process_counts[process.id] += 1
+                process.outputs.forEach(input => {
+                    materials.add(input.mul(1));
+                });
+                process.inputs.forEach(input => {
+                    let required_for_count = input.mul(1);
+                    materials.sub(required_for_count);
+                });
+            }
+            current = materials.min_total(imported_materials);
+        }
+        this.materials = materials;
+        this.process_counts = process_counts;
+    }
+
+    // target is to produce stack
+    // imported_materials are allowed to have any -ive material count.
+    // exported_materials are allowed to have any +ive material count.
+    update0(stack, imported_materials, exported_materials) {
+        let process_counts = this.processes.reduce(
+            (acc, cur) => {
+                acc[cur.id] = 1;
+                return acc;
+            },
+            {}
+        );
+
+        // let process_counts = {};
+        // process_counts['coolant'] = 6;
+        // process_counts['coolant'] *= 21.43/30;
+        // process_counts['liquid_mineral_oil_catalyst'] = 1;
+        // process_counts['liquid_mineral_oil_catalyst'] *= 8.88 / 10;
+        // process_counts['residual_oil_refining'] = 1;
+        // process_counts['residual_oil_refining'] *= (10/3)/(20/3);
+        // process_counts['residual_oil_refining'] *= (10/4)/(10/3);
+        // process_counts['residual_oil_refining'] *= (2.3)/(2.5);
+        // process_counts['residual_oil_refining'] *= (2.2)/(2.3);
+        // process_counts['carbon_separation_1'] = 1;
+        // process_counts['carbon_separation_1'] *= 2.96/25;
+        // process_counts['steam_cracking_oil_residual'] = 1;
+        // process_counts['steam_cracking_oil_residual'] *= (1/3)/(5/2);
+        // process_counts['steam_cracking_oil_residual'] *= (1/4)/(1/3);
+        // process_counts['steam_cracking_oil_residual'] *= (0.23)/(1/4);
+        // process_counts['steam_cracking_oil_residual'] *= (0.22)/(0.23);
+        // process_counts['carbon'] = 1;
+        // process_counts['carbon'] *= 0.06/1;
+        // process_counts['oil_refining'] = 1;
+        // process_counts['angels_steam_water'] = 1;
+        // process_counts['angels_steam_water'] *= 2.2/40;
+
+        let materials = new StackSet();
+        this.processes.forEach(p => {
+            p.outputs.forEach(input => {
+                materials.add(input.mul(process_counts[p.id]));
+            });
+            p.inputs.forEach(input => {
+                materials.sub(input.mul(process_counts[p.id]));
+            });
+        });
+        let margins = materials.margins([].concat(imported_materials).concat(exported_materials));
+        let min_stack = margins.min_total();
+        let total_p = materials.total_positive(min_stack.item);
+        let total_n = materials.total_negative(min_stack.item);
+        let process = this.processes_by_input[min_stack.item.id][0]; // XXX assume first only.
+        let multiplier = 1;
+        if (total_p.quantity < (total_n.quantity*-1)) {
+            multiplier = total_p.quantity / total_n.quantity * -1;
+        } else {
+            multiplier = total_n.quantity / total_p.quantity * -1;
+        }
+        process_counts[process.id] *= multiplier;
+
+        this.materials = materials;
+        this.process_counts = process_counts;
+    }
+
+    // target is to produce stack
+    // imported_materials are allowed to have any -ive material count.
+    // exported_materials are allowed to have any +ive material count.
+    update3(stack, imported_materials, exported_materials) {
+        let process_counts = this.processes.reduce(
+            (acc, cur) => {
+                acc[cur.id] = 1;
+                return acc;
+            },
+            {}
+        );
+
+        let materials = new StackSet();
+        let iterations = 10000;
+        while (iterations-- > 0) { // effectively `while (true)`
+            materials = new StackSet();
+            this.processes.forEach(p => {
+                p.outputs.forEach(input => {
+                    materials.add(input.mul(process_counts[p.id]));
+                });
+                p.inputs.forEach(input => {
+                    materials.sub(input.mul(process_counts[p.id]));
+                });
+            });
+            let margins = materials.margins([].concat(imported_materials).concat(exported_materials));
+            let min_stack = margins.min_total();
+            if (min_stack.quantity > -0.0001) break;
+            let total_p = materials.total_positive(min_stack.item);
+            let total_n = materials.total_negative(min_stack.item);
+            let process = this.processes_by_input[min_stack.item.id][0]; // XXX assume first only.
+            let multiplier = 1;
+            if (total_p.quantity < (total_n.quantity*-1)) {
+                multiplier = total_p.quantity / total_n.quantity * -1;
+            } else {
+                multiplier = total_n.quantity / total_p.quantity * -1;
+            }
+            process_counts[process.id] *= multiplier;
+        }
+
+
+        // iterations = 10000;
+        // while (iterations-- > 0) { // effectively `while (true)`
+        //     materials = new StackSet();
+        //     this.processes.forEach(p => {
+        //         p.outputs.forEach(input => {
+        //             materials.add(input.mul(process_counts[p.id]));
+        //         });
+        //         p.inputs.forEach(input => {
+        //             materials.sub(input.mul(process_counts[p.id]));
+        //         });
+        //     });
+        //     let margins = materials.margins([].concat(imported_materials).concat(exported_materials));
+        //     let max_stack = margins.max_total();
+        //     if (max_stack.quantity < 0.0001) break;
+        //     let total_p = materials.total_positive(max_stack.item);
+        //     let total_n = materials.total_negative(max_stack.item);
+        //     let process = this.processes_by_input[max_stack.item.id][0]; // XXX assume first only.
+        //     let multiplier = 1;
+        //     if (total_p.quantity < (total_n.quantity*-1)) {
+        //         multiplier = total_p.quantity / total_n.quantity * -1;
+        //     } else {
+        //         multiplier = total_n.quantity / total_p.quantity * -1;
+        //     }
+        //     process_counts[process.id] *= multiplier;
+        // }
+
+        // console.error("=== Margins ===");
+        // Object.values(margins.stacks).forEach(m => {
+        //     console.error(m[0].item.id, m[0].quantity);
+        // });
+        // console.error("=== Process Counts ===");
+        // Object.entries(process_counts).forEach(pc => {
+        //     console.error(pc[0] + " => " + pc[1]);
+        // });
+
+        this.materials = materials;
+        this.process_counts = process_counts;
+    }
+
+    // target is to produce stack
+    // imported_materials are allowed to have any -ive material count.
+    // exported_materials are allowed to have any +ive material count.
+    update4(stack, imported_materials, exported_materials) {
+        let process_counts = this.processes.reduce(
+            (acc, cur) => {
+                acc[cur.id] = 1;
+                return acc;
+            },
+            {}
+        );
+
+        let materials = new StackSet();
+        let total_iterations = 10000;
+        let iterations = total_iterations;
+        while (iterations-- > 0) { // `while (true)` with a limit.
+            materials = new StackSet();
+            this.processes.forEach(p => {
+                p.outputs.forEach(input => {
+                    materials.add(input.mul(process_counts[p.id]));
+                });
+                p.inputs.forEach(input => {
+                    materials.sub(input.mul(process_counts[p.id]));
+                });
+            });
+
+        //     {
+        // this.materials = materials;
+        // this.process_counts = process_counts;
+        // let iter = '' + (total_iterations-iterations);
+        // while (iter.length < ('' + total_iterations).length) iter = '0' + iter;
+        // let f = path.join(__dirname, '..', 'iterations', 'iter_' + iter + '.gv');
+        // fs.writeFileSync(f, this.to_graphviz());
+        //     }
+
+            let margins = materials.margins_squared([].concat(imported_materials).concat(exported_materials));
+            let stack_to_balance = margins.max_total();
+            if (stack_to_balance.quantity < 0.0001) break;
+            let item_to_balance = stack_to_balance.item;
+            let total_p = materials.total_positive(item_to_balance).quantity; // produced
+            let total_n = materials.total_negative(item_to_balance).quantity*-1; // consumed
+            let multiplier = (total_p / total_n) ** 0.5; // p.m = c.m; p/c = m**2; m = (p/c)**0.5
+            let processes_in = this.processes_by_input[item_to_balance.id]; // processes that take this type as an input
+            let processes_out = this.processes_by_output[item_to_balance.id]; // processes that have this type as an output
+            processes_in.forEach(p => {
+                process_counts[p.id] *= multiplier;
+            });
+            processes_out.forEach(p => {
+                process_counts[p.id] /= multiplier;
+            });
+        }
+
         this.materials = materials;
         this.process_counts = process_counts;
     }
