0ddfbc5ce2e7e2275942a39d6bb6968996441bda
candle <candle@candle.me.uk>
2021-05-11 09:51:50 +0100

    Add voxel tycoon.

---
+++ src/vt/data.js
@@ -0,0 +1,51 @@
+import { Data, Item, Stack, FactoryGroup, Factory, Process } from '../structures.js';
+
+import * as fs from 'fs'
+import * as path from 'path'
+import { fileURLToPath } from 'url';
+const __dirname = path.dirname(fileURLToPath(import.meta.url));
+let raw = JSON.parse(fs.readFileSync(path.join(__dirname, 'vt.json'), 'utf8'));
+
+const check_add = function(item, fn) {
+    try {
+        return fn();
+    } catch (error) {
+        console.log("error processing item:", item);
+        throw error;
+    }
+}
+
+let data = new Data('Voxel Tycoon', '0.0.1');
+
+raw.items.forEach(i => check_add(i, () => {
+    if (!i.id) return;
+    data.add_item(new Item(i.id, i.i18n.en, i.group))
+}));
+
+raw.processes.forEach(p => {
+    if (!p.name) return;
+    if (!data.factory_groups[p.factory_group]) {
+        check_add(p, () => data.add_factory_group(new FactoryGroup(p.factory_group)));
+    }
+    let inputs = Object.entries(p.inputs).map((e) => {
+        return check_add(p, () => new Stack(data.items[e[0]], e[1]));
+    });
+    let outputs = Object.entries(p.outputs).map((e) => {
+        return check_add(p, () => new Stack(data.items[e[0]], e[1]));
+    });
+    check_add(p, () => {
+        data.add_process(new Process(
+            p.name,
+            inputs,
+            outputs,
+            p.duration,
+            check_add(p, () => data.factory_groups[p.factory_group])
+    ))});
+})
+
+
+data.add_factories([
+    new Factory('', '', data.factory_groups.alloy_smelter, 1),
+]);
+
+export { data };
