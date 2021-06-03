0b36bf8178c13c02fc5934c7b7bdec98506bb68e
candle <candle@candle.me.uk>
2021-06-03 11:00:48 +0100

    copper plates using coolant

--- src/index-factorio.js
+++ src/index-factorio.js
@@ -1,61 +1,7 @@
-// import { data } from './dsp/data.js';
-// import { data } from './vt/data.js'
 import { data } from './factorio-ab-01/data.js'
-//import { data } from './factorio-ab/data.js'
 import { ProcessChain, RateChain, Stack } from './structures.js';
-// import { inspect } from 'util'
 const readline = import('readline');
 
-
-// console.log(inspect(data, false, null, true));
-
-// console.log(data);
-
-// let p = new ProcessChain([
-//     data.processes.waste_water_purification_sulfuric,
-//     data.processes.acid_sulfuric,
-//     data.processes.gas_sulfur_dioxide,
-//     data.processes.iron_ore_by_sorting,
-//     data.processes.mineral_catalyst,
-//     data.processes.mineral_sludge_from_slag_slurry,
-//     data.processes.slag_slurry_from_crushed_stone,
-//     data.processes.ore_jivolite_crushing,
-//     data.processes.ore_saphirite_crushing,
-// ]);
-
-
-// Object.entries(data.processes).forEach(([id, proc]) => console.log(id));
-
-// const readline_disambiguate = function(requirement, options) {
-//     console.log('Multiple potential options for ', requirement, ':', options);
-
-// };
-
-// const array_disambiguate = function(requirement, options) { }
-// const array_disambiguate = function(requirement, options) {
-//     let arr = {};
-//     arr[data.items.Diamond.id] = data.processes.Diamond_Rare;
-//     arr[data.items.Carbon_Nanotube.id] = data.processes.Carbon_Nanotube;
-//     arr[data.items.Graphene.id] = data.processes.Graphene_Rare;
-//     arr[data.items.Casimir_Crystal.id] = data.processes.Casimir_Crystal;
-//     arr[data.items.Photon_Combiner.id] = data.processes.Photon_Combiner;
-//     arr[data.items.Energetic_Graphite.id] = data.processes.Energetic_Graphite;
-//     arr[data.items.Crystal_Silicon.id] = data.processes.Crystal_Silicon_Rare;
-//     arr[data.items.Deuterium.id] = data.processes.Deuterium;
-//     if (arr[requirement] === false) {
-//         throw new Error('No enabled priority for ' + requirement + ' (available: ' + options.map(i => i.id).join(', ') + ')');
-//     }
-//     return arr[requirement];
-// };
-
-// const array_disambiguate = function(requirement, options) {
-//     let arr = {};
-//     arr[data.items.gas_nitrogen_monoxide.id] = data.processes.solid_sodium_nitrate_processing;
-//     if (!arr[requirement] === false) {
-//         throw new Error('No enabled priority for ' + requirement + ' (available: ' + options.map(i => i.id).join(', ') + ')');
-//     }
-//     return arr[requirement];
-// };
 
 const array_disambiguate = function(requirement, options) {
     let arr = {};
@@ -74,7 +20,6 @@ const array_disambiguate = function(requirement, options) {
     arr[data.items.titanium_plate.id] = data.processes.angels_roll_titanium_converting;
     arr[data.items.angels_roll_titanium.id] = data.processes.roll_titanium_casting_fast;
 
-    // advanced:
     arr[data.items.liquid_molten_titanium.id] = data.processes.molten_titanium_smelting_4;
     arr[data.items.ingot_titanium.id] = data.processes.sponge_titanium_smelting;
     arr[data.items.liquid_titanium_tetrachloride.id] = data.processes.processed_titanium_smelting;
@@ -122,6 +67,11 @@ const array_disambiguate = function(requirement, options) {
     arr[data.items.liquid_concrete.id] = data.processes.concrete_mixture_2;
     arr[data.items.solid_cement.id] = data.processes.cement_mixture_1;
 
+    // arr[data.items.copper_plate.id] = data.processes.angels_plate_copper;
+    arr[data.items.copper_plate.id] = data.processes.angels_roll_copper_converting;
+    arr[data.items.angels_roll_copper.id] = data.processes.roll_copper_casting_fast;
+    arr[data.items.ingot_copper.id] = data.processes.anode_copper_smelting;
+
     if (!(arr[requirement]) === true) {
         throw new Error('No enabled priority for ' + requirement + ' (available: ' + options.map(i => i.id).join(', ') + ')\n\n'
             + options.map(p => {
@@ -142,29 +92,9 @@ const array_disambiguate = function(requirement, options) {
 };
 
 
-// let names = [
-//     'iron_plate',
-//     'magnet',
-//     'gear',
-//     'magnetic_coil',
-//     'elctric_motor',
-//     'electromagnetic_turbine',
-//     'copper_plate',
-//     'particle_container',
-// ];
-// let p = new ProcessChain(names.map(n => data.processes[n]));
-// let p = new ProcessChain([data.processes['diamond']])
 let p = new ProcessChain(Object.values(data.processes))
-    // .disable(data.processes.Sulphuric_Acid.id)
-    // .disable(data.processes.Silicon_Ore.id)
-    // .filter_for_output(new Stack(data.items.U_Matrix, 1), array_disambiguate)
-    // .disable(data.processes.Silicon_Ore.id)
-    // .disable(data.processes.Sulphuric_Acid.id)
-    // .filter_for_output(new Stack(data.items.Small_Carrier_Rocket, 1), array_disambiguate)
-    // .filter_for_output(new Stack(data.items.electric_motor, 1), array_disambiguate)
-    // .filter_for_output(new Stack(data.items.graviton_lens, 1), array_disambiguate)
     .filter_for_output(
-        new Stack(data.items.concrete_brick, 1),
+        new Stack(data.items.copper_plate, 1),
         array_disambiguate,
         [
             data.items.geode_blue.id,
@@ -201,23 +131,12 @@ let p = new ProcessChain(Object.values(data.processes))
             data.items.quartz.id,
             data.items.solid_lime.id,
             data.items.water_thin_mud.id,
+            data.items.copper_ore.id,
+            data.items.liquid_coolant.id,
         ]
     )
-    // .enable(data.processes.residual_oil_refining)
-    // .enable(data.processes.steam_cracking_oil_residual)
-    // .enable(data.processes.liquid_mineral_oil_catalyst)
-    // .enable(data.processes.carbon_separation_1)
-    // .enable()
-    // .enable()
-    // .enable()
-    // .enable(data.processes.solid_salt_from_saline)
-    // .enable(data.processes.greenyellow_waste_water_purification)
-    // .filter_for_output(new Stack(data.items['liquid_sulfuric_acid'], 1), array_disambiguate)
-    // .filter_for_output(new Stack(data.items.circuit, 1), array_disambiguate)
-    // .filter_for_output(new Stack(data.items.liquid_nitric_acid, 1), array_disambiguate)
     ;
 
-// p = new RateChain(p, {'assembler': data.factories.assembler_III});
 p = new RateChain(p, {
     'ore_sorting': data.factories['ore_crusher_3'],
     'ore_sorting_t1': data.factories['ore_sorting_facility_4'],
@@ -248,11 +167,14 @@ p = new RateChain(p, {
     'strand_casting_3': data.factories.strand_casting_machine_4,
     'strand_casting_4': data.factories.strand_casting_machine_4,
     'advanced_crafting': data.factories.assembling_machine_6,
+    'chemical_smelting': data.factories.angels_chemical_furnace_4,
+    'chemical_smelting_2': data.factories.angels_chemical_furnace_4,
+    'chemical_smelting_3': data.factories.angels_chemical_furnace_4,
+    'chemical_smelting_4': data.factories.angels_chemical_furnace_4,
+    'petrochem_electrolyser': data.factories.angels_electrolyser_4,
 });
 
-console.error(data.factories);
-// // // let r = p.update(new Stack(data.items.circuit, 10));
-p.update(new Stack(data.items.concrete_brick, 10),
+p.update(new Stack(data.items.copper_plate, 120),
     [// imported
         data.items.water.id,
         data.items.water_purified.id,
@@ -267,41 +189,13 @@ p.update(new Stack(data.items.concrete_brick, 10),
         data.items.catalysator_orange.id,
         data.items.sulfur.id,
         data.items.steel_plate.id,
+        data.items.copper_ore.id,
+        data.items.liquid_coolant.id,
     ],
     [// exported
         data.items.nickel_ore.id,
-        data.items.liquid_coolant.id,
     ]
 );
-// p.update(new Stack(data.items['liquid_sulfuric_acid'], 600));
-// p.update(new Stack(data.items['liquid_nitric_acid'], 100));
-// p.update(new Stack(data.items.electric_motor, 8));
-// p.update(
-//     new Stack(data.items.Small_Carrier_Rocket, 2),
-//     [
-    // data.items.Hydrogen.id, data.items.Silicon_Ore.id, data.items.Sulphuric_Acid.id]
-//     );
-// console.log(r[0], r[1]);
-// let p = new ProcessChain(Object.entries(data.processes).flatMap(
-//  ([id, proc]) => proc
-// ));
 
-// let processes_from_exported_data = [
-// 'angelsore-crushed-mix1-processing', // iron ore from 2x crushed + catalyst.
-// ]
-
-//console.log(p.processes_by_output)
-// console.log('circuit production rate', data.processes[3].production_rate(data.items.circuit, 1))
-
-// console.log(p.require_output(new Stack(data.items.circuit, 10)));
-// console.log(p.all_items());
 
 console.log(p.to_graphviz());
-
-
-// TITANIUM
-// ALUMINIUM (bauxite)
-// SILVER
-// COBALT
-
-// (URANIUM)
