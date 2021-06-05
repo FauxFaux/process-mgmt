80ade7b1ba4f94188ec9070f3b9ec83945c2ac69
candle <candle@candle.me.uk>
2021-06-05 16:45:17 +0100

    script to add items

---
+++ add_item.sh
@@ -0,0 +1,30 @@
+#!/bin/bash
+
+set -euxo pipefail
+
+FILE="$1"
+FILE_TMP_BASE=".1.$FILE"
+FILE_TMP_ADD=".2.$FILE"
+
+cp "$FILE" "$FILE_TMP_BASE"
+
+while true; do
+    id=""
+    name=""
+    echo "Name"
+    read name
+    echo "id"
+    read id
+
+    if [ -z "$id" -o -z "$name" ]; then
+        echo "exiting"
+        mv "$FILE_TMP_BASE" "$FILE"
+        exit
+    fi
+
+    jq -r \
+        '.items += [{ "id": "'"$id"'", "i18n": { "en": "'"$name"'"  } }]'\
+        "$FILE_TMP_BASE" > "$FILE_TMP_ADD"
+
+    mv "$FILE_TMP_ADD" "$FILE_TMP_BASE"
+done
