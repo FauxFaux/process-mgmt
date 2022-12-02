befefdecbcad2b956230b58acdd06f095a90be0e
candle <candle@candle.me.uk>
2022-12-02 22:46:39 +0000

    make factory-rate honour selected factory types

--- src/index.js
+++ src/index.js
@@ -251,7 +251,7 @@ const command_rate = function(argv) {
                     [].concat(config.get_imported()).concat(config.get_exported())
                     ))
                 .accept(new RateVisitor(process => {
-                    let f = quickest_factory_for_factory_type(data, process.factory_group);
+                    let f = config.get_factory_type(data, process.id, () => quickest_factory_for_factory_type(data, process.factory_group).modify(speed, output));
                     if ((typeof f) === "undefined") {
                         console.warn("No factory found for ", process.factory_group);
                         f = new Factory('__default__', '__default__', null, 1, 1);
