867f3d9f83c1a3a34edecaebf10e14c6f0fe5721
candle <candle@candle.me.uk>
2024-03-02 14:06:23 +0000

    add new modapack; start to refactor factorio recipe-lister data sets

---
+++ src/factorio-1.1.104-very-bz-0.5.1/data.js
@@ -0,0 +1,12 @@
+
+import { default as create_data } from '../factorio-recipe-lister/data_base.js';
+
+const _import_file = function(name) {
+    return import('./recipe-lister/' + name, {assert: { type: 'json'}})
+        .catch(e => {
+        console.log('failed to read recipe.json:', e);
+    })
+    .then(m => m.default)
+};
+
+export default await create_data('factorio-1.1.104-very-bz-0.5.1', '0.0.1', _import_file);
