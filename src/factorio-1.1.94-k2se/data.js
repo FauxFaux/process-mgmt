b5ec5010db40a202691ae9b0d7b1a3b3d0c7c803
Chris West <git@goeswhere.com>
2024-03-26 18:35:50 +0000

    feat: add k2se (#7)

---
+++ src/factorio-1.1.94-k2se/data.js
@@ -0,0 +1,15 @@
+import { default as create_data } from '../factorio-recipe-lister/data_base.js';
+
+const _import_file = function (name) {
+    return import('./recipe-lister/' + name, { assert: { type: 'json' } })
+        .catch((e) => {
+            console.log('failed to read recipe.json:', e);
+        })
+        .then((m) => m.default);
+};
+
+export default await create_data(
+    'factorio-1.1.94-k2se',
+    '0.0.1',
+    _import_file,
+);
